d4b9d468f00ef22cad7994cd24f652b224bc76b9
Velizar25 <>
2021-11-25 08:12:25 +0000

    Some message

--- src/exercise7/RecursivePrimePrinter.java
+++ src/exercise7/RecursivePrimePrinter.java
@@ -13,20 +13,18 @@ public class RecursivePrimePrinter {
     }
 
     public static void printPrimes(int n) {
-        // Implement recursive method
         for (int i = n; i > 0; i--) {
-          if (isPrime(n)) {
-            System.out.println(i);
-          }
+            if (isPrime(i)) {
+                System.out.println(i);
+            }
         }
     }
 
     public static boolean isPrime(int n) {
-        // Implement method
         for (int i = 2; i < n; i++) {
-          if (n % i == 0) {
-            return false;
-          }    
+            if (n % i == 0) {
+                return false;
+            }
         }
         return true;
     }
